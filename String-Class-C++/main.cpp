93c6357e5726d022d3d9590565f143e43ce27ed2
Sofiane Medhouny Laouina <sofiane.medhouny.laouina@gmail.com>
2024-12-04 20:27:41 +0100

    Initial Commit

---
+++ String-Class-C++/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  String-Class-C++
+//
+//  Created by Sofiane Medhouny Laouina on 04/12/2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
